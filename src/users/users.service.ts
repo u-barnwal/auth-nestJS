ed1cf91c3d1b00583946a7a7273458fef7257cdf
Utsav Barnwal <u.barnwal36@gmail.com>
2022-01-23 20:38:24 +0530

    write user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Harry',
+      username: 'harry',
+      password: 'expelliarmus',
+    },
+    {
+      id: 1,
+      name: 'Ron',
+      username: 'ron',
+      password: 'what?',
+    },
+    {
+      id: 1,
+      name: 'Hermione',
+      username: 'hermione',
+      password: 'no.',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
