4772e7ae49a7a5b7df355f3a0a88a6dd8e85f721
Utsav Barnwal <u.barnwal36@gmail.com>
2022-01-23 21:22:56 +0530

    install and use express-session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
+
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   console.log(`Started on http://localhost:${port}`);
 }
