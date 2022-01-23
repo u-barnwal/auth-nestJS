4f22f92eb4e4fa4c0a3da9c7849445435b85bb93
Utsav Barnwal <u.barnwal36@gmail.com>
2022-01-23 20:44:03 +0530

    create auth module & service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.password === password) {
+      delete user.password;
+      delete user.username;
+
+      return user;
+    }
+
+    return null;
+  }
+}
